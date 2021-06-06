73a44c6c4d142e705cabff10bf95391be3b20ba6
sergio vera <Sergio Vera>
2021-06-06 17:03:24 +0200

    version

--- apitasks/main.go
+++ apitasks/main.go
@@ -2,14 +2,15 @@ package main
 
 import (
 	"log"
-    "net/http"
-	"github.com/vsergio011/apitasks/routes"
+	"net/http"
 
+	"github.com/vsergio011/apitasks/routes"
 )
+
+const version string = "1.0.0"
 
 func main() {
 	s := routes.New()
 	log.Fatal(http.ListenAndServe(":8080", s.Router()))
-  
-}
 
+}
